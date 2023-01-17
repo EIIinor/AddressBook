1a24b768a1f7e5139c540129e536f7382091c9dd
EIIinor <ellinor.ljus@utb.ecutbildning.se>
2023-01-17 13:43:15 +0100

    Removing a contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,25 +4,10 @@ using Newtonsoft.Json;
 
 var menu = new Menu();
 menu.FilePath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\content.json";
-var contacts = new List<Contact>();
-var FileService = new FileService();
 
-PopulateContactList();
 
 while (true)
 {
     Console.Clear();
     menu.OptionsMenu();
-}
-
-
-void PopulateContactList()
-{
-    try
-    {
-        var content = FileService.ReadFromFile();
-        if (!string.IsNullOrEmpty(content))
-            contacts = JsonConvert.DeserializeObject<List<Contact>>(content);
-    }
-    catch { }
 }
--- AddressBook/Services/Menu.cs
+++ AddressBook/Services/Menu.cs
@@ -1,8 +1,10 @@
 ﻿using AddressBook.Interfaces;
 using AddressBook.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Newtonsoft.Json;
 using System;
 using System.Collections.ObjectModel;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace AddressBook.Services;
@@ -10,24 +12,33 @@ namespace AddressBook.Services;
 internal class Menu
 {
 
-    public List<Contact> contacts = new List<Contact>();
-    public FileService file = new FileService();
+    private List<Contact> contacts = new List<Contact>();
+    private FileService file = new FileService();
 
-    //public string FilePath { get; set; } = null!;
 
     public string FilePath { get; set; } = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}.\content.json";
 
+    void PopulateContactList()
+    {
+        try
+        {
+            var content = file.ReadFromFile();
+            if (!string.IsNullOrEmpty(content))
+                contacts = JsonConvert.DeserializeObject<List<Contact>>(content);
+        }
+        catch { }
+    }
 
     public void OptionsMenu()
     {
-        
+        PopulateContactList();
 
         Console.Clear();
         Console.WriteLine("Welcome to the Address Book");
         Console.WriteLine("1. Create a new contact");
         Console.WriteLine("2. Show all contacts");
         Console.WriteLine("3. Show a specific contact");
-        Console.WriteLine("4. Delete a specefic contact");
+        Console.WriteLine("4. Remove a specefic contact");
         Console.WriteLine("Select one of the options above: ");
         var option = Console.ReadLine();
 
@@ -43,7 +54,7 @@ internal class Menu
     private void OptionOne()
     {
         Console.Clear();
-        Console.WriteLine("1. Create a new contact");
+        Console.WriteLine("1. Create a new contact:\n");
 
         var contact = new Contact();
 
@@ -84,7 +95,8 @@ internal class Menu
         Console.WriteLine("Here are the current people in your address book:\n");
         foreach (var contact in contacts)
         {
-            Console.WriteLine(contact.DisplayName);
+            Console.WriteLine($"{contact.FirstName} , {contact.LastName}, {contact.Email}");
+            Console.WriteLine();
         }
         Console.WriteLine("\nPress any key to continue.");
         Console.ReadKey();
@@ -92,55 +104,55 @@ internal class Menu
 
 
 
-    private void OptionThree(/*string FirstName*/)
+    private void OptionThree()
     {
         Console.Clear();
-        Console.WriteLine("3. Show one contact");
+        Console.WriteLine("3. Show one contact:\n");
 
-        //var contact = contacts.FirstOrDefault(c => c.FirstName == FirstName);
-        //if (contact == null)
-        //{
-        //    Console.WriteLine("Contact not found");
-        //}
-        //else
-        //{
-        //    Console.WriteLine($"Contact: {contact.FirstName}, {contact.LastName}, {contact.Email}, {contact.Phone}, {contact.StreetName} , {contact.City}, {contact.PostalCode}");
-        //}
+        var contact = contacts.FirstOrDefault(contact => contact.FirstName == contact.FirstName);
+        if (contact == null)
+        {
+            Console.WriteLine("Contact not found");
+        }
+        else
+        {
+            Console.WriteLine ($"Firstname: {contact.FirstName}");
+            Console.WriteLine($"Lastname: {contact.LastName}");
+            Console.WriteLine($"Phone number: {contact.Phone}");
+            Console.WriteLine($"Email: {contact.Email}");
+            Console.WriteLine($"Address: {contact.StreetName} , {contact.City}, {contact.PostalCode}");
+        }
 
-
-        //var contactService = new ContactService();
-        //var contact = ContactService.Get(x => x.Id == Guid.Parse("ccb186fc-e462-4002-b3c0-e4b56774bc54"));
-        //Console.WriteLine(contact.FirstName);
-
+        Console.WriteLine("\nPress any key to continue.");
         Console.ReadLine();
     }
 
- 
 
-    private void OptionFour(/*Contact contact*/)
+
+
+    private void OptionFour()
     {
         Console.Clear();
-        Console.WriteLine("4. Remove a specific contact");
+        Console.WriteLine("4. Remove a specific contact:\n");
 
+        Console.WriteLine("Enter the first name of the person you would like to remove.");
+        string firstName = Console.ReadLine();
+        var Contact = contacts.FirstOrDefault(contact => contact.FirstName.ToLower() == firstName.ToLower());
 
-        //Console.WriteLine("Enter the first name of the person you would like to remove.");
-        //string firstName = Console.ReadLine();
-        //Contact contact = Contact.FirstOrDefault(x => x.FirstName.ToLower() == FirstName.ToLower());
+        if (Contact == null)
+        {
+            Console.WriteLine("That person could not be found. Press any key to continue");
+            Console.ReadKey();
+            return;
+        }
+        Console.WriteLine("Are you sure you want to remove this person from your address book? (Y/N)");
+        if (Console.ReadLine().ToLower() == "y")
+        {
+            contacts.Remove(Contact);
+            Console.WriteLine("Person removed. Press any key to continue.");
+            Console.ReadKey();
 
-        //if (contact == null)
-        //{
-        //    Console.WriteLine("That person could not be found. Press any key to continue");
-        //    Console.ReadKey();
-        //    return;
-        //}
-        //Console.WriteLine("Are you sure you want to remove this person from your address book? (Y/N)");
-        //Contact(contact);
-
-        //if (Console.ReadKey().Key == ConsoleKey.Y)
-        //{
-        //    Contacts.Remove(Contact);
-        //    Console.WriteLine("Person removed. Press any key to continue.");
-        //    Console.ReadKey();
-        //}
+            file.SaveToFile(JsonConvert.SerializeObject(contacts));
+        }
     }
 }
